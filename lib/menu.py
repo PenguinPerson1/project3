bee50424df7904ca375d66e7d13b26a7ea45a489
Nia <nia.plossl@gmail.com>
2024-05-01 16:22:52 -0400

    Split choose_option
    split choose_option into 2 methods. choose_option works the same, but return_option returns whatever input they gave

--- lib/menu.py
+++ lib/menu.py
@@ -1,17 +1,24 @@
 class Menu:
     @classmethod
-    def str_range(self,max):
+    def str_range(cls,max):
         return [str(i+1) for i in range(max)]
 
     @classmethod
-    def choose_option(self,options:list,results:list):
-        user_input = ''
+    def choose_option(cls,options:list,results:list):
+        
+        user_input = cls.return_option(options)
 
+        for i in range(len(options)):
+            if user_input == options[i]:
+                results[i]()
+
+    @classmethod
+    def return_option(cls,options:list):
+        user_input = ''
+        # options is a list of any string you will accept
         while user_input not in options:
             user_input = input(">>> ")
             if user_input not in options:
                 print("Invalid option please enter a number listed")
 
-        for i in range(len(options)):
-            if user_input == options[i]:
-                results[i]()
+        return user_input
